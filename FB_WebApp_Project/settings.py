0d73f493712b3edd0e9d5061bf0f5ed63499fe3e
ToR_Aekkasit <aekkasit.se@gmail.com>
2025-05-24 19:34:30 +0700

    Update settings.py

--- FB_WebApp_Project/settings.py
+++ FB_WebApp_Project/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATICFILES_DIRS = [
+  BASE_DIR / "static",
+  BASE_DIR / "assets", 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
