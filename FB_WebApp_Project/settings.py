b99fe20cb8cf5a132b7a6aaaabcdf3bcf4f17fe1
ToR_Aekkasit <aekkasit.se@gmail.com>
2025-05-24 20:17:11 +0700

    Update settings.py

--- FB_WebApp_Project/settings.py
+++ FB_WebApp_Project/settings.py
@@ -55,8 +55,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'django.middleware.security.SecurityMiddleware',
-  'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
 ]
 
 ROOT_URLCONF = "FB_WebApp_Project.urls"
