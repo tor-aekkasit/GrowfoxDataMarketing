7a570c130d543e5769b0120d093b3b685c4bd4ac
blackbook75 <maythas@growfox.co>
2025-05-27 15:47:22 +0700

    Update forms.py

--- PageInfo/forms.py
+++ PageInfo/forms.py
@@ -2,15 +2,20 @@ from django import forms
 from .models import PageGroup
 
 class PageURLForm(forms.Form):
-    url = forms.URLField(label="Page URL", required=True)
+    url = forms.URLField(
+        label="Page URL",
+        widget=forms.URLInput(attrs={
+            'class': 'form-control form-control-lg',  # ใส่ขนาด input ใหญ่เหมือนหน้า add_page
+            'placeholder': 'Input URL Page'
+        })
+    )
 
 class PageGroupForm(forms.ModelForm):
     class Meta:
         model = PageGroup
         fields = ['group_name']
         widgets = {
-            'page_name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter Group Name'
+            'group_name': forms.TextInput(attrs={
+                'class': 'form-control form-control-lg',  # เพิ่ม form-control-lg เพื่อให้ input ใหญ่เหมือน add_page
+                'placeholder': 'Input Group Name'
             }),
-        }
