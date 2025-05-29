febbf3127474865aba09965d976abdb0725dd3ef
blackbook75 <maythas@growfox.co>
2025-05-29 13:05:51 +0700

    Update views.py

--- PageInfo/views.py
+++ PageInfo/views.py
@@ -23,30 +23,59 @@ def add_page(request, group_id):
         form = PageURLForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data['url']
-            fb_data = FBPageInfo(url)
+            platform = form.cleaned_data['platform']  # เพิ่ม platform ในฟอร์ม
 
-            if 'page_id' in fb_data:
-                follower_data = PageFollowers(fb_data['page_id'])
-                if follower_data:
-                    fb_data.update(follower_data)
+            if platform == 'facebook':
+                fb_data = FBPageInfo(url)
+                if 'page_id' in fb_data:
+                    follower_data = PageFollowers(fb_data['page_id'])
+                    if follower_data:
+                        fb_data.update(follower_data)
 
-            allowed_fields = {f.name for f in PageInfo._meta.get_fields()}
-            filtered_data = {k: v for k, v in fb_data.items() if k in allowed_fields}
+                allowed_fields = {f.name for f in PageInfo._meta.get_fields()}
+                filtered_data = {k: v for k, v in fb_data.items() if k in allowed_fields}
 
-            for key in ['page_likes_count', 'page_followers_count']:
-                value = filtered_data.get(key)
-                if isinstance(value, str):
-                    filtered_data[key] = int(value.replace(',', ''))
-                elif isinstance(value, int):
-                    filtered_data[key] = value
+                for key in ['page_likes_count', 'page_followers_count']:
+                    value = filtered_data.get(key)
+                    if isinstance(value, str):
+                        filtered_data[key] = int(value.replace(',', ''))
+                    elif isinstance(value, int):
+                        filtered_data[key] = value
 
-            PageInfo.objects.create(page_group=group, **filtered_data)
+                filtered_data['platform'] = 'facebook'  # เพิ่ม platform ใน record
+                PageInfo.objects.create(page_group=group, **filtered_data)
+
+            elif platform == 'tiktok':
+                from .tiktok_page_info import TikTokPageInfo
+                tiktok_data = TikTokPageInfo(url)
+
+                if tiktok_data:
+                    # Mapping TikTok fields to PageInfo fields
+                    allowed_fields = {f.name for f in PageInfo._meta.get_fields()}
+                    filtered_data = {
+                        'page_username': tiktok_data.get('username'),
+                        'page_name': tiktok_data.get('page_name'),
+                        'page_followers': tiktok_data.get('followers'),
+                        'page_likes': tiktok_data.get('likes'),
+                        'page_description': tiktok_data.get('bio'),
+                        'profile_pic': tiktok_data.get('profile_pic'),
+                        'page_url': tiktok_data.get('url'),
+                        'platform': 'tiktok'
+                    }
+                    # ตรวจสอบว่าแต่ละ field อยู่ใน allowed_fields
+                    filtered_data = {k: v for k, v in filtered_data.items() if k in allowed_fields}
+
+                    PageInfo.objects.create(page_group=group, **filtered_data)
+                else:
+                    form.add_error(None, "Failed to fetch TikTok data.")
+                    return render(request, 'PageInfo/add_page.html', {'form': form, 'group': group})
+
             return redirect('group_detail', group_id=group.id)
+
     else:
         form = PageURLForm()
 
     return render(request, 'PageInfo/add_page.html', {'form': form, 'group': group})
-
 def group_detail(request, group_id):
     group = PageGroup.objects.get(id=group_id)
     pages = group.pages.all()  # ต้องมี related_name='pages'
